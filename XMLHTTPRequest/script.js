84053ea6ed5f620c8f7008a462ce1a7a85c9cd85
Manuel_Rivera <infernaldragon91@gmail.com>
2022-10-27 16:00:50 -0600

    Actualización
    Actualización de la enseñanza de XMLHTTP añadiendo la posibilidad de uso de un navegador que no detecte AJAX

--- XMLHTTPRequest/script.js
+++ XMLHTTPRequest/script.js
@@ -1,3 +1,30 @@
-const peticion = new XMLHttpRequest
+if(window.XMLHttpRequest){
+    peticion = new XMLHttpRequest();
 
-console.log(peticion)
+} else {
+    peticion = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+
+
+
+// Aquí revisa que se haya entregado la información y se valida si lo encontró
+    peticion.addEventListener("load",()=>{
+        let respuesta;
+        if(peticion.readyState == 4 && peticion.status == 200){
+            respuesta = peticion.response
+        } else{
+            respuesta = "Lo siento, no se ha encontrado el recurso, MAQUINOLA";
+        } console.log(respuesta);
+    })
+    peticion.open("GET","informacion.txt");
+    peticion.send();
+
+    
+    /*
+console.log(peticion)*/
+/*
+    peticion.addEventListener("readystatechange",()=>{
+        console.log(peticion.readyState);
+        console.log(peticion.response);
+    })*/
